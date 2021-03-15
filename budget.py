bb4d61a36ce66f8fd159873f09983de80e341e1f
tpemartin <tpemartin@gmail.com>
2021-03-16 06:02:16 +0800

    new

---
+++ budget.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+def budget_sheet():
+    webbrowser.open_new("https://docs.google.com/spreadsheets/d/1ixnSU5dz1og-Q9WGosOhnPZiJGEGP2IZBMeMUSna2GQ/edit#gid=0")
+
+def duplicate():
+
+
+
+budget_sheet()
